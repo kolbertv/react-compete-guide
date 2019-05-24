ddd0e7b0eef3bf3ab53f30863bead46fa7a9488e
kolbert vladimir <kolbert@yandex.ru>
2019-05-24 16:51:32 +0300

    67

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -14,27 +14,47 @@ class App extends Component {
 
 const BoilingVerdict = props => {
   if (props.celsius >= 100) {
-    return <p>Вода закипит</p>;
+    return <p> Вода закипит </p>;
   }
-  return <p>Вода не закипит</p>;
+  return <p> Вода не закипит </p>;
 };
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { temperature: '' };
+    this.state = {
+      temperature: 'c'
+    };
   }
 
-  handleChange = e => {
-    this.setState({
-      temperature: e.target.value
-    });
+  handleCelsiusChange = temperature => {
+    this.setState({ scale: 'c', temperature });
   };
+
+  handleFahrenheitChange = temperature => {
+    this.setState({ scale: 'f', temperature });
+  };
+
   render() {
+    const scale = this.state.scale;
+    const temperature = this.state.temperature;
+    const celsius =
+      scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+    const fahrenheit =
+      scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
+
     return (
       <div>
-        <TemperatureInput scale="c" />
-        <TemperatureInput scale="f" />
+        <TemperatureInput
+          scale="c"
+          temperature={celsius}
+          onTemperatureChange={this.handleCelsiusChange}
+        />
+        <TemperatureInput
+          scale="f"
+          temperature={fahrenheit}
+          onTemperatureChange={this.handleFahrenheitChange}
+        />
       </div>
     );
   }
@@ -66,7 +86,9 @@ const tryConvert = (temperature, convert) => {
 class TemperatureInput extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { temperature: '' };
+    this.state = {
+      temperature: ''
+    };
   }
   handleChange = e => {
     this.props.onTemperatureChange(e.target.value);
@@ -76,7 +98,7 @@ class TemperatureInput extends React.Component {
     const scale = this.props.scale;
     return (
       <fieldset>
-        <legend>Введите температуру в градусах {scaleNames[scale]}</legend>
+        <legend> Введите температуру в градусах {scaleNames[scale]} </legend>
         <input value={temperature} onChange={this.handleChange} />
       </fieldset>
     );
